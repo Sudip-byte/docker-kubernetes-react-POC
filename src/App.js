5ab42e03270287746dde9da901bffe51161d48be
SUDIP RANA <sudiprana2997@gmail.com>
2022-06-04 18:48:59 +0530

    Change App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sudip Rana : IBM INDIA Pvt Ltd
+        learn react
         </p>
         
       </header>
